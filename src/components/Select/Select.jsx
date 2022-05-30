dd0b447454de4edaf48565290bfa37fb329a51e0
Mysik <Mishamysik@gmail.com>
2022-05-30 08:08:03 +0300

    fix close select when click outsie

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,9 +1,10 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import './select.css';
 import {Mode} from "../../context/Modes/Modes";
 import {Game} from "../../context/GameLogic/GameLogic";
 
 const Select = () => {
+    const ref = useRef();
     const [isSelectActive, setIsSelectActive] = useState(false);
 
     const {modes, modePick, setModePick} = useContext(Mode);
@@ -20,8 +21,20 @@ const Select = () => {
         }
     };
 
+    useEffect(() => {
+        const checkIfClickedOutside = e => {
+            if (isSelectActive && ref.current && !ref.current?.contains(e.target)) {
+                setIsSelectActive(false)
+            }
+        }
+        document.addEventListener("mousedown", checkIfClickedOutside)
+        return () => {
+            document.removeEventListener("mousedown", checkIfClickedOutside)
+        }
+    }, [isSelectActive]);
+
     return (
-        <div className='app--select select'>
+        <div className='app--select select' ref={ref}>
             <div className={`select--pick ${isSelectActive && 'select--pick__active'} ${modePick?.name && 'select--pick__selected'} ${isGameStart && 'select--pick__disabled'} `} onClick={selectHandler}>
                 {modePick?.name || 'Pick Mode' }
                 <span className={`select--triangle ${isSelectActive && 'select--triangle__active'}`}/>
